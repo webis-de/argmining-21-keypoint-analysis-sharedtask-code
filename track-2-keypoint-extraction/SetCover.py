9a945b0023032a241506a25f30781c53bd7ed4fd
phhei <philipp.heinisch@googlemail.com>
2021-06-26 23:48:07 +0200

    Hyperparemeters more clear - formatting update

--- track-2-keypoint-extraction/SetCover.py
+++ track-2-keypoint-extraction/SetCover.py
@@ -8,6 +8,12 @@ import gensim.downloader
 
 from Utils import UserLabelCluster
 
+# hyperparameters
+max_length_of_keypoint = 100
+max_number_of_keypoints = 10
+threshold_bert_similarity = .6
+
+
 if __name__ == '__main__':
     model = gensim.downloader.load("glove-wiki-gigaword-300")
 
@@ -27,16 +33,16 @@ if __name__ == '__main__':
 
             all_arguments_including_cost = [(a[0],
                                              frozenset([c.get_index(asa) for asa in a[1]]),
-                                             1+len(a)/100. +
+                                             1+len(a)/float(max_length_of_keypoint) +
                                              (3 if a[0].startswith("I ") else (2 if a[0].startswith("In my opinion")
                                                                                else (.5 if a[0].startswith("\"")
                                                                                      else 0)))) for a in all_arguments
-                                            if len(a[0]) < 100]
+                                            if len(a[0]) < max_length_of_keypoint]
             set_of_keypoints = set()
             covered_aspects = set()
 
             coverage_list = all_arguments_including_cost
-            for i in range(10):
+            for i in range(max_number_of_keypoints):
                 coverage_list = [(a[0], frozenset(a[1].difference(covered_aspects)), a[2], len(a[1])) for a in coverage_list]
                 coverage_list.sort(key=lambda e: (len(e[1]), -e[2]-e[3]), reverse=True)
                 add_elem = coverage_list.pop(0)
@@ -51,14 +57,17 @@ if __name__ == '__main__':
                                        rescale_with_baseline=True, idf=True, lang="en")
                 to_remove = []
                 for cand in range(ret[-1].shape[0]):
-                    if cand != keypoint_index and  ret[-1][cand] >= .6:
+                    if cand != keypoint_index and ret[-1][cand] >= threshold_bert_similarity:
                         print("\"{}\" and \"{}\" are similar - discard the second one".format(keypoint[0], candidates[cand][0]))
                         to_remove.append(candidates[cand] if cand > keypoint_index else keypoint)
                 for r in to_remove:
                     candidates.remove(r)
 
             print("Best combination for topic \"{}\"->{} is: {}".format(topic, stance,
-                                                                        " ++ ".join(map(lambda x: "{} ({})".format(*x[:2],), candidates))))
+                                                                        " ++ ".join(
+                                                                            map(lambda x: "{} ({})".format(*x[:2]),
+                                                                                candidates)))
+                  )
 
             final_topic_stance_keypoint_dict[(topic, stance)] = \
                 [re.sub(string=cand[0], pattern="^[Ii]n my (personal)? opinion,\s*", repl="") for cand in candidates]
